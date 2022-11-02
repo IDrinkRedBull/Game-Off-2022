ffd379a0a69258fd5b654989126780925dcdf1c1
NihaoMan <100158885+NihaoMan@users.noreply.github.com>
2022-11-02 16:40:05 +0100

    movement and abit change

---
+++ Game Off 2022/Assets/scipt/Movement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float horizonetal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    [SerializeField] private Rigidbody2D apple;
+    [SerializeField] private Transform groundX;
+    [SerializeField] private LayerMask groundZ;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizonetal = Input.GetAxisRaw("Horizontal");
+        flip();
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            apple.velocity = new Vector2(apple.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && apple.velocity.y > 0f)
+        {
+            apple.velocity = new Vector2(apple.velocity.x, apple.velocity.y * 0.5f);
+        }
+        //Debug.Log(apple.velocity);
+
+    }
+
+    private bool IsGrounded()// bug fix
+    {
+        return Physics2D.OverlapCircle(groundX.position, 0.2f, groundZ);
+    }
+
+    private void FixedUpdate()
+    {
+        apple.velocity = new Vector2(horizonetal * speed, apple.velocity.y);
+    }
+
+    private void flip()
+    {
+        if (isFacingRight && horizonetal < 0f || !isFacingRight && horizonetal > 0f)
+        {
+
+
+            isFacingRight = !isFacingRight;
+            transform.Rotate(0f, 180f, 0f);
+            /*
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+            */
+        }
+    }
+}
